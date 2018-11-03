da9fe03e037669495d3d1908741af07020e312af
Sviatoslav78 <gaevoy70@gmail.com>
2018-11-03 17:36:50 +0200

    firstHomework

---
+++ src/com/company/Main.java
@@ -0,0 +1,21 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String first = "\n\tI like summer very much. This doesn’t mean that I don’t like spring, winter or autumn, but the fact\n";
+        String second = "that you do not need to get up early to go to school and you needn’t do your homework every day\n";
+        String third = "makes summer my favorite season.\n";
+        String fourth = "\tDuring my summer holidays, I do everything I like most of all. I play football, spend time with my\n";
+        String fifth = "parents and friends.\n";
+        String sixth = "\tUsually, I spend my summer holidays in the country. I like growing fruit and vegetables. My dad always\n";
+        String seventh = "tells me something interesting about plants and nature.\n";
+        String eighth = "\tI like to walk in the summer forest with my friends or with my parents. We often go to the river. This\n";
+        String ninth = "year we went fishing with my dad. We got up early, took our fishing rods and went to the nearest lake.\n";
+        String tenth = "parents and friends.\n";
+        String eleventh = "We caught a lot of fish. That was amazing.\n";
+        System.out.println(first  + second + third + fourth + fifth + sixth + seventh + eighth + ninth + tenth + eleventh);
+
+
+    }
+}
